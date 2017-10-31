3719eb27739f3bf4257e6524c4e0ee7d76d5e0cb
Mr.Miner <dev@mrminer.co>
2017-10-31 18:28:29 +0300

    Initial commit

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,4 @@ source "/root/mrminer/lib/functions.sh"
 
 boot
 
-<<<<<<< HEAD
-sudo -H -u mrminer /root/mrminer/boot/prepare.sh &
-#su mrminer -c 'bash /root/mrminer/boot/prepare.sh' &
-=======
-su mrminer -c 'bash /root/mrminer/boot/prepare.sh' &
->>>>>>> ef77ff7b40e0f66cf98bbfc466627e5533f1a211
+sudo -u mrminer /root/mrminer/boot/prepare.sh &
