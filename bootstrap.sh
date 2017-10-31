d12a9f919a08f5c2f37e6fdd3f29a2652a001ddc
Mr.Miner <dev@mrminer.co>
2017-10-31 18:46:38 +0300

    asd

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,6 @@ source "/root/mrminer/lib/functions.sh"
 
 boot
 
-sudo -u mrminer -s sh -c '/root/mrminer/boot/prepare.sh' &
+#sudo -u mrminer -s sh -c '/root/mrminer/boot/prepare.sh' &
 #sudo -u mrminer /root/mrminer/boot/prepare.sh &
+su miner -c 'bash /root/mrminer/boot/prepare.sh' &
