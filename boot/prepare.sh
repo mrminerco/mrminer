1fd0a61e3b649b509eff36aed30de62ede12fdba
Mr.Miner <dev@mrminer.co>
2017-10-31 19:59:07 +0300

    asd

--- boot/prepare.sh
+++ boot/prepare.sh
@@ -5,15 +5,13 @@ export DISPLAY=:0
 export GPU_MAX_ALLOC_PERCENT=100
 export GPU_USE_SYNC_OBJECTS=1
 export GPU_SINGLE_ALLOC_PERCENT=100
-export GPU_MAX_HEAP_SIZE=100
-export GPU_FORCE_64BIT_PTR=1
+
 
 # Screen Cleaning
-clear
-killall screen -9 &
-screen -wipe &
+killall screen -9
+screen -wipe
 clear
 sleep 1
 
 # Register Screen
-screen -dm -S mrminer bash -c "sudo /root/mrminer/boot/configure.sh" &
+screen -dm -S mrminer bash -c "/root/mrminer/boot/configure.sh" &
