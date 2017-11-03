3c9f3d8cd8984639271f72e7a034eeff9a206c37
Mr.Miner <dev@mrminer.co>
2017-11-03 23:47:24 +0300

    asd

---
+++ cron/update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CheckUpdate=$(sudo git --work-tree=/root/mrminer --git-dir=/root/mrminer/.git remote update)
+
+if CheckUpdate; then
+
+  Local=$(sudo git --work-tree=/root/mrminer --git-dir=/root/mrminer/.git rev-list --max-count=1 master)
+  Origin=$(sudo git --work-tree=/root/mrminer --git-dir=/root/mrminer/.git rev-list --max-count=1 origin/master)
+
+  if [ "$Local" != "$Origin" ]; then
+      sudo git --work-tree=/root/mrminer --git-dir=/root/mrminer/.git checkout -- .
+      sleep 0.5
+      sudo git --work-tree=/root/mrminer --git-dir=/root/mrminer/.git pull origin master
+      sync
+  fi
+
+fi
