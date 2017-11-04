48b0a4344606d96764c471f6e45464da1aed95ff
Mr.Miner <dev@mrminer.co>
2017-11-04 04:08:52 +0300

    a

--- boot/configure.sh
+++ boot/configure.sh
@@ -21,6 +21,7 @@ function task1()
 		printf "[   ] Connection Test ( Waiting Network... %ss )\r" $(text yellow "$x")
 		LanIP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 		if [ -n "$LanIP" ]; then
+			printf "\n"
 			x=100
 		fi
 		x=$((x + 1))
