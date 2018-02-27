bc981cdccc63c5ff68c8f9e2cd5c848535986ba2
Mr.Miner <dev@mrminer.co>
2018-02-27 14:47:04 +0300

    Nvidia Support

--- tool/hardware.sh
+++ tool/hardware.sh
@@ -2,11 +2,25 @@
 
 LAN_IP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 
+WAN_IP=$(curl ifconfig.me)
+
 UPTIME=$(uptime -s)
 
 KERNEL_VERSION=$(uname -r)
 
-DRIVER_VERSION=$(sudo dpkg-query --showformat='${Version}' --show amdgpu-pro)
+# MRMINER_VERSION=$(cat /home/mrminer/version)
+
+DRIVER_AMD=$(lsmod | grep amd | wc -l)
+DRIVER_NVIDIA=$(lsmod | grep nvidia | wc -l)
+
+if [ $DRIVER_AMD -gt 0 ]; then
+	DRIVER=AMD
+	DRIVER_VERSION=$(sudo dpkg-query --showformat='${Version}' --show amdgpu-pro-core)
+elif [ $DRIVER_NVIDIA -gt 0 ]; then
+	DRIVER=NVIDIA
+	DRIVER_VERSION=$(sudo dpkg-query --showformat='${Version}' --show nvidia-390)
+fi
+
 
 MOBO_BRAND=$(sudo dmidecode -s baseboard-manufacturer)
 MOBO_MODEL=$(sudo dmidecode -s baseboard-product-name)
@@ -20,9 +34,18 @@ HDD=$(sudo lshw -C disk -short | tail -n +3 | head -n 1 | awk '{$1=$2=$3=""}1' |
 
 SLOT=$(sudo dmidecode -t slot | grep "Designation:" | cut -b 15- | awk '{printf("%s ", $NF)}')
 
-GPUS=$(/root/mrminer/tool/amdmeminfo -s -o -q | tr '\n' ',' | sed 's/.$//')
 
-GPU_COUNT=$(ls -1 /sys/class/drm/card*/device/hwmon/hwmon*/pwm1 | wc -l)
+if [ "$DRIVER" == "AMD" ]; then
+
+	GPUS=$(/root/mrminer/tool/amdmeminfo -s -o -q | tr '\n' ',' | sed 's/.$//')
+	GPU_COUNT=$(ls -1 /sys/class/drm/card*/device/hwmon/hwmon*/pwm1 | wc -l)
+
+elif [ "$DRIVER" == "NVIDIA" ]; then
+
+	GPUS=$(nvidia-smi -L | tr '\n' ',')
+	GPU_COUNT=$(nvidia-smi -L | wc -l)
+
+fi
 
 HARDWARE={}
 
--- tool/overclock.sh
+++ tool/overclock.sh
@@ -3,20 +3,41 @@
 source "/root/mrminer/lib/functions.sh"
 updateconfig
 
-x=0;
-while [ $x -le 14 ]; do
-    if [ -e "/sys/class/drm/card$x/device/pp_table" ]; then
 
-        cp /var/tmp/pp_tables/gpu$x/pp_table /sys/class/drm/card$x/device/pp_table
-        sleep 0.2
-        echo manual > /sys/class/drm/card$x/device/power_dpm_force_performance_level
-        mem_states=`cat /sys/class/drm/card$x/device/pp_dpm_mclk | wc -l`
-        MEMSTATE=$(($mem_states-1))
-        sudo /root/mrminer/tool/ohgodatool -i $x --core-state $POWER --mem-state $MEMSTATE --core-clock $CORE --mem-clock $MEMORY --volt-state $POWER --vddc-table-set $VOLT
-        sleep 0.2
-        echo $POWER > /sys/class/drm/card$x/device/pp_dpm_sclk
-        echo $MEMSTATE > /sys/class/drm/card$x/device/pp_dpm_mclk
+if [ "$DRIVER" == "AMD" ]; then
 
-    fi
-    x=$[x + 1]
-done
+    IFS=',' read -r -a CoreArray <<< "$CORE"
+    IFS=',' read -r -a MemArray <<< "$MEMORY"
+
+    x=0;
+    while [ $x -le 14 ]; do
+        if [ -e "/sys/class/drm/card$x/device/pp_table" ]; then
+
+            cp /var/tmp/pp_tables/gpu$x/pp_table /sys/class/drm/card$x/device/pp_table
+            sleep 0.2
+            echo manual > /sys/class/drm/card$x/device/power_dpm_force_performance_level
+            mem_states=`cat /sys/class/drm/card$x/device/pp_dpm_mclk | wc -l`
+            MEMSTATE=$(($mem_states-1))
+
+            if [ ! -z "${CoreArray[$x]}" ] && [ ! -z "${MemArray[$x]}" ]; then
+                sudo /root/mrminer/tool/ohgodatool -i $x --core-state $POWER --mem-state $MEMSTATE --core-clock ${CoreArray[$x]} --mem-clock ${MemArray[$x]} --volt-state $POWER --vddc-table-set $VOLT
+            else
+                sudo /root/mrminer/tool/ohgodatool -i $x --core-state $POWER --mem-state $MEMSTATE --core-clock $CORE --mem-clock $MEMORY --volt-state $POWER --vddc-table-set $VOLT
+            fi
+
+            sleep 0.2
+            echo $POWER > /sys/class/drm/card$x/device/pp_dpm_sclk
+            echo $MEMSTATE > /sys/class/drm/card$x/device/pp_dpm_mclk
+
+        fi
+        x=$[x + 1]
+    done
+
+elif [ "$DRIVER" == "NVIDIA" ]; then
+
+  IFS=',' read -r -a CoreArray <<< "$CORE"
+  IFS=',' read -r -a MemArray <<< "$MEMORY"
+
+  sudo xinit /root/mrminer/tool/overclock_nvidia.sh $CoreArray $MemArray $POWER -- :0 -once -config /etc/X11/xorg.conf &
+
+fi
