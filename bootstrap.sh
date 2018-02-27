5d0354d2a12ba57110ae0ff5aa1f853f674f5a27
Mr.Miner <dev@mrminer.co>
2018-02-27 12:04:26 +0300

    Nvidia Support

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,21 @@ source "/root/mrminer/lib/functions.sh"
 
 boot
 
-su mrminer -c 'bash /root/mrminer/boot/prepare.sh' &
+if [ "$DRIVER" == "AMD" ]; then
+
+	su mrminer -c 'bash /root/mrminer/boot/prepare.sh' &
+
+elif [ "$DRIVER" == "NVIDIA" ]; then
+
+	sudo nvidia-xconfig -s -a --force-generate --allow-empty-initial-configuration --cool-bits=31 --registry-dwords="PerfLevelSrc=0x2222" --no-sli --connected-monitor="DFP-0"
+	sudo sed -i '/Driver/a \ \ \ \ Option         "Interactive" "False"' /etc/X11/xorg.conf
+
+	sleep 1
+
+	su mrminer -c 'bash /root/mrminer/boot/prepare.sh' &
+
+else
+
+	su mrminer -c 'bash /root/mrminer/boot/prepare.sh' &
+
+fi
