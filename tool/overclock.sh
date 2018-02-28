282586b3d0423334b01925d23765178ab5c6c492
Mr.Miner <dev@mrminer.co>
2018-02-28 16:44:00 +0300

    Improved

--- tool/overclock.sh
+++ tool/overclock.sh
@@ -6,49 +6,45 @@ updateconfig
 if [ "$DRIVER" == "AMD" ]; then
 
     GPU_COUNT=$(ls -1 /sys/class/drm/card*/device/hwmon/hwmon*/pwm1 | wc -l)
-    IFS=',' read -r -a CoreArray <<< "$CORE"
-    IFS=',' read -r -a MemArray <<< "$MEMORY"
+    IFS=',' read -r -a Cores <<< "$CORE"
+    IFS=',' read -r -a Mems <<< "$MEMORY"
 
-    [[ ! -z $CoreArray ]] &&
-    CoreArray=($CoreArray) &&
-    for (( x=${#CoreArray[@]}; x < $GPU_COUNT; ++x )); do
-    	CoreArray[$x]=${CoreArray[$x-1]}
+    # Fix rest of values
+    [ -n "$Cores" ] &&
+    for (( x=${#Cores[@]}; x < $GPU_COUNT; ++x )); do
+    	Cores[$x]=${Cores[$x-1]}
+    done
+    [ -n "$Mems" ] &&
+    for (( x=${#Mems[@]}; x < $GPU_COUNT; ++x )); do
+    	Mems[$x]=${Mems[$x-1]}
     done
 
-    [[ ! -z $MemArray ]] &&
-    MemArray=($MemArray) &&
-    for (( x=${#MemArray[@]}; x < $GPU_COUNT; ++x )); do
-    	MemArray[$x]=${MemArray[$x-1]}
-    done
+    # Overclock Loop
+    i=0
+    CARDS=$(ls -d1 /sys/class/drm/card*/device/pp_table | grep -Poi "(\d+)" | xargs)
+    for ID in $CARDS; do
 
+      parameters=''
+      cp /var/tmp/pp_tables/gpu$ID/pp_table /sys/class/drm/card$ID/device/pp_table && sleep 0.2
+      echo manual > /sys/class/drm/card$ID/device/power_dpm_force_performance_level
+      MEMSTATE=$(/root/mrminer/tool/ohgodatool -i $ID --show-mem | grep -o -P "Memory state [0-9]" | grep -o "[[:digit:]]" | tail -n 1)
 
-    i=0;
-  	g=0;
-  	while [ $g -le 16 ]; do
+      if [ -n "${Cores[$i]}" ] && [ "${Cores[$i]}" -ge "300" ] && [ "${Cores[$i]}" -le "1600" ]; then
+          parameters+=" --core-state $POWER --core-clock ${Cores[$i]}"
+      fi
+      if [ -n "${Mems[$i]}" ] && [ "${Mems[$i]}" -ge "300" ] && [ "${Mems[$i]}" -le "3000" ]; then
+          parameters+=" --mem-state $MEMSTATE --mem-clock ${Mems[$i]}"
+      fi
+      if [ "$VOLT" -ge "800" ] && [ "$VOLT" -le "1300" ]; then
+          parameters+=" --volt-state $POWER --vddc-table-set $VOLT"
+      fi
 
-        if [ -e "/sys/class/drm/card$g/device/pp_table" ]; then
+      [ -n "$parameters" ] && sudo /root/mrminer/tool/ohgodatool -i $ID $parameters
+      [ -n "$POWER" ] && echo $POWER > /sys/class/drm/card$ID/device/pp_dpm_sclk
+      [ -n "$MEMSTATE" ] && echo $MEMSTATE > /sys/class/drm/card$ID/device/pp_dpm_mclk
 
-            cp /var/tmp/pp_tables/gpu$g/pp_table /sys/class/drm/card$g/device/pp_table
-            sleep 0.2
-            echo manual > /sys/class/drm/card$g/device/power_dpm_force_performance_level
-            mem_states=`cat /sys/class/drm/card$g/device/pp_dpm_mclk | wc -l`
-            MEMSTATE=$(($mem_states-1))
+      i=$[i + 1]
 
-            if [ ! -z "${CoreArray[$i]}" ] && [ ! -z "${MemArray[$i]}" ]; then
-                sudo /root/mrminer/tool/ohgodatool -i $i --core-state $POWER --mem-state $MEMSTATE --core-clock ${CoreArray[$i]} --mem-clock ${MemArray[$i]} --volt-state $POWER --vddc-table-set $VOLT
-            else
-                sudo /root/mrminer/tool/ohgodatool -i $i --core-state $POWER --mem-state $MEMSTATE --core-clock $CORE --mem-clock $MEMORY --volt-state $POWER --vddc-table-set $VOLT
-            fi
-
-            sleep 0.2
-            echo $POWER > /sys/class/drm/card$i/device/pp_dpm_sclk
-            echo $MEMSTATE > /sys/class/drm/card$i/device/pp_dpm_mclk
-
-            i=$[i + 1]
-
-        fi
-
-        g=$[g + 1]
     done
 
 elif [ "$DRIVER" == "NVIDIA" ]; then
